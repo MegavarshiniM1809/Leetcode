66042e8e3f3808560b2627b5d5d0a17d650aff8f
Megavarshini <megavarshini1809@gmail.com>
2025-09-26 09:27:24 +0530

    Time: 44 ms (61.54%), Space: 93.9 MB (66.44%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -4,7 +4,7 @@ static bool cmplt(vector<int>&a, vector<int> &b){
     return a[1]<b[1];
 }
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        int cet=-50000;
+        int cet=INT_MIN;
         int removedCount=0;
         sort(intervals.begin(),intervals.end(), cmplt);
         for(int i=0;i<intervals.size();i++){
