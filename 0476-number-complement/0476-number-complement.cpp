dc35187c224fb8607a6391ddc0da5f357e61cf25
Megavarshini <megavarshini1809@gmail.com>
2025-12-22 22:15:35 +0530

    Time: 0 ms (100%), Space: 8 MB (16.23%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findComplement(int num) {
+         int count=1;
+       int temp=num;
+      while(temp!=1 && temp!=0){
+        temp=temp/2;
+        count++;
+      }  
+      int res=num^(int)(pow(2,count)-1);
+      return res; 
+    }
+};
