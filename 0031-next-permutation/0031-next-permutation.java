e4cbcfdfceb203a464caa8ea7eeeba8a8aa5dba4
Megavarshini <megavarshini1809@gmail.com>
2026-08-20 15:59:46 +0530

    Time: 1 ms (9.07%), Space: 44.8 MB (33.85%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n=nums.length;
+        int idx = -1;
+        for(int i=n-1;i>0;i--){
+            if(nums[i-1]<nums[i]){
+                idx = i-1;
+                break;
+            }
+        }
+        if (idx==-1){
+            Arrays.sort(nums);
+            return;
+        }
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]>nums[idx]){
+                int temp = nums[i];
+                nums[i] = nums[idx];
+                nums[idx] = temp;
+                break;
+            }
+        }
+        for(int i=idx+1;i<n;i++){
+            int j=n-1;
+            while(i<=j){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+                j--;
+            }
+            break;
+        }
+        
+    }
+}
